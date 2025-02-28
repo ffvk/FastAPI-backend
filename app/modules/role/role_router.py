64c200b4a2cb4ef360c7fa8e8efae87d85bdc061
dhirtiroy321 <dhriti@ovntech.com>
2025-02-28 18:01:56 +0530

    role done

--- app/modules/role/role_router.py
+++ app/modules/role/role_router.py
@@ -4,7 +4,8 @@ from app.db.session import get_db
 from app.utils.auth import get_current_user
 from typing import Optional
 from app.modules.user.user_model import UserModel
-from app.modules.role.role_service import get_all_roles
+from app.modules.role.role_schema import CreateRoleSchema, UpdateRoleSchema, DeleteRoleSchema
+from app.modules.role.role_service import get_all_roles,get_role_by_id, create_role, update_role, delete_existing_role
 
 router = APIRouter()
 
@@ -13,3 +14,55 @@ def get_all_roles_endpoint(db: Session = Depends(get_db),current_user: UserModel
     
     roles_data = get_all_roles(db=db, current_user=current_user)
     return roles_data
+
+
+@router.get("/{role_id}",status_code=status.HTTP_200_OK)
+def get_role_by_id_endpoint (role_id:int, db:Session = Depends(get_db),current_user: UserModel = Depends(get_current_user)):
+
+    get_role_data = get_role_by_id(db=db, role_id=role_id,current_user=current_user)
+
+    return get_role_data
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_new_role_endpoint(
+    newRole:CreateRoleSchema,
+    db: Session=Depends(get_db),
+    current_user: UserModel= Depends(get_current_user)
+    
+    ):
+   
+    new_role = create_role(db=db,role_service_data=newRole,current_user=current_user)
+        
+    return new_role
+
+
+@router.put("/{role_id}", status_code=status.HTTP_200_OK)
+async def update_existing_role_endpoint(
+    role_id: int,
+    role_update: UpdateRoleSchema,
+    db: Session = Depends(get_db),
+    current_user: UserModel = Depends(get_current_user)
+):
+    updated_role = update_role(
+        db=db, role_id=role_id, role_service_data=role_update, current_user=current_user
+    )
+    
+    return updated_role
+
+
+@router.delete("/{role_id}",  response_model= DeleteRoleSchema)
+def delete_role_endpoint(
+    role_id: int,
+    db: Session = Depends(get_db),
+    current_user: UserModel = Depends(get_current_user)
+    ):
+    """
+    Soft delete a order item by ID.
+    """
+   
+    delete_existing_role(db=db, role_id=role_id, current_user=current_user)
+    
+    
+    return {"message" : "Order item deleted successfully",}
+
